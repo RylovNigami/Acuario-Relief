828ac88413c8e7ed7ae74e48ec154b6ce78f5d73
RylovNigami <mendezr1700@gmail.com>
2023-07-14 21:29:26 -0400

    (first commit) feat: adding chart.js, typescript & a littel view of a chart on the main page

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-page class="flex flex-center">
+    <Bar :data="data" :options="options" />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+import { Bar } from 'vue-chartjs'
+import * as chartConfig from './chartConfig.ts'
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
+export default defineComponent({
+  name: 'IndexPage , App',
+  components: {
+    Bar
+  },
+  data() {
+    return chartConfig
+  }
+}
+)
+</script>
